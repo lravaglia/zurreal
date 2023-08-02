2e47b32a81c336788bdb4400fa02f3671821654e
Lorenzo Ravaglia <lorenzo.ravaglia@hope.edu>
2023-08-02 18:14:41 -0400

    feat: apply tables schemas to connection

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,16 @@ import table from "@/table";
 
 export default class Zurreal extends Surreal {
   /**
-   *
+   * When provided with an array of table names and zod object schemas, this method converts the zod schemas to surrealql statements and runs them against the database to create schemafull tables.
    * @param tables An array of table names and the zod object  schema defining fields and contraints.
    * @requires The connection MUST have a user logged in with the authority to create tables to the desired database.
    */
   public async defineTableSchemas(
     tables: { name: string; schema: z.AnyZodObject }[]
   ) {
-    tables.map(({ name, schema }) => table(name, schema));
+    const schemas = await Promise.all(
+      tables.map(({ name, schema }) => table(name, schema))
+    );
+    await Promise.all(schemas.map((s) => this.query(s)));
   }
 }
